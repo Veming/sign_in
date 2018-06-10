0b18b9518c887ccad51a78dedd4e77fe8446e438
Veming <1159288455@qq.com>
2018-06-10 21:58:48 +0800

    修改文件

--- src/main/java/com/hrbust/su/sign_in/dao/StudentDao.java
+++ src/main/java/com/hrbust/su/sign_in/dao/StudentDao.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface StudentDao extends JpaRepository<Student, String> {
 
 
-    @Query(value = "select t from t_student t where t.real_name = ?1 and t.id_card = ?2")
+    @Query(value = "select t from Student t where t.realName = ?1 and t.idCard = ?2")
     public List<Student> getUserInfo(String realName, String idNbr);
 }
